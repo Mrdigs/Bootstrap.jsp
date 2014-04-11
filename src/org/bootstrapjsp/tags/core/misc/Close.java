081cfce36328d71fbb222fe393bd96952913ae1b
Mrdigs <digger.scott@gmail.com>
2014-04-11 08:44:43 +0100

    Added new close component to encapsulate the bootstrap close button (as opposed to a mold for the button component)

---
+++ src/org/bootstrapjsp/tags/core/misc/Close.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2014 Darren Scott - All Rights Reserved
+ * 
+ * This program is distributed under LGPL Version 2.1 in the hope that
+ * it will be useful, but WITHOUT ANY WARRANTY.
+ */
+package org.bootstrapjsp.tags.core.misc;
+
+import org.bootstrapjsp.dialect.Aria;
+import org.bootstrapjsp.dialect.Html;
+import org.bootstrapjsp.tags.html.Span;
+import org.tldgen.annotations.BodyContent;
+import org.tldgen.annotations.Tag;
+
+@Tag(bodyContent=BodyContent.EMPTY,dynamicAttributes=true)
+public class Close extends Span {
+
+	public Close() {
+		super(Html.BUTTON_ELEMENT);
+		super.setAttribute(Html.TYPE_ATTRIBUTE, Html.BUTTON_ELEMENT);
+		super.setAttribute(Html.CLASS_ATTRIBUTE, "close");
+		super.setAttribute(Aria.HIDDEN, "true");
+		super.setBody("&times;");
+	}
+
+}
