ba285086c46a48ae2797047dbdafaf43e6239de3
Mrdigs <digger.scott@gmail.com>
2014-04-12 19:22:21 +0100

    Mold property setting supports bool parameter types

--- src/org/bootstrapjsp/util/ComponentUtil.java
+++ src/org/bootstrapjsp/util/ComponentUtil.java
@@ -1,5 +1,7 @@
 package org.bootstrapjsp.util;
 
+import java.lang.reflect.Method;
+
 import org.bootstrapjsp.tags.Component;
 
 public class ComponentUtil {
@@ -9,5 +11,29 @@ public class ComponentUtil {
 		final String packageName = clazz.getPackage().getName();
 		final String componentName = clazz.getName().replace(packageName + ".", "");
 		return componentName.toLowerCase();
+	}
+	
+	public static boolean setProperty(Component component, String property, String value) {
+		final StringBuilder methodNameBuilder = new StringBuilder("set");
+		methodNameBuilder.append(property.substring(0, 1).toUpperCase());
+		methodNameBuilder.append(property.substring(1));
+		final String setMethodName = methodNameBuilder.toString();
+		try {
+			for (Method setMethod :component.getClass().getMethods()) {
+				if (setMethod.getName().equals(setMethodName)) {
+					final Class<?> parameterType = setMethod.getParameterTypes()[1];
+					if (String.class.equals(parameterType)) {
+						setMethod.invoke(component, new Object[] {value});
+					} else if (Boolean.class.equals(parameterType)) {
+						final Boolean bool = Boolean.valueOf(value);
+						setMethod.invoke(component, new Object[] {bool});
+					}
+					return true;
+				}
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		return false;
 	}
 }
