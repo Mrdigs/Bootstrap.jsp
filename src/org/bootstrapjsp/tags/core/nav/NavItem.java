3022711418fca3e04ff5412b61e3aad0966a7e11
Mrdigs <digger.scott@gmail.com>
2014-04-15 07:29:33 +0100

    Fixed up NavItem to support either body or label inside its anchor

--- src/org/bootstrapjsp/tags/core/nav/NavItem.java
+++ src/org/bootstrapjsp/tags/core/nav/NavItem.java
@@ -9,14 +9,18 @@ package org.bootstrapjsp.tags.core.nav;
 import org.bootstrapjsp.facet.ActiveFacet;
 import org.bootstrapjsp.facet.DisabledFacet;
 import org.bootstrapjsp.facet.LabelFacet;
+import org.bootstrapjsp.facet.Labelable;
+import org.bootstrapjsp.tags.ext.Message;
 import org.bootstrapjsp.tags.html.Anchor;
 import org.bootstrapjsp.tags.html.ListItem;
 import org.tldgen.annotations.Attribute;
 import org.tldgen.annotations.Tag;
 
 @Tag(name="navitem",dynamicAttributes=true)
-public class NavItem extends ListItem {
+public class NavItem extends ListItem implements Labelable {
 
+	private Anchor anchor = null;
+	
 	public NavItem() {
 		super();
 		super.addFacet(new ActiveFacet(false));
@@ -26,23 +30,36 @@ public class NavItem extends ListItem {
 	
 	/**
 	 * Sets the href for this Nav Item. Setting this will automatically
-	 * enclose the body in an &lt;a href="[href]"&gt;. If the href contains
-	 * a hash, then the data-toggle attribute is automatically set to "tab",
-	 * and if the href also does not start with a hash, if adds a data-target
-	 * attribute with the fragment identifier as the value.
+	 * enclose the body in an &lt;a href="[href]"&gt;. Unless the Nav is
+	 * a Navbar Nav, then if the href contains a hash, then the data-toggle 
+	 * attribute is automatically set to "tab", and if the href also does 
+	 * not start with a hash, it adds a data-target attribute with the 
+	 * fragment identifier as the value.
 	 */
 	@Attribute(rtexprvalue=true)
 	public void setHref(String href) {
-		final Anchor anchor = new Anchor(); 
-		if (href.contains("#")) {
-			final String target = href.split("#")[1];
-			anchor.setAttribute("data-toggle", "tab");
-			if (!target.equals(href)) {
-				anchor.setAttribute("data-target", "#" + target);
+		this.anchor = new Anchor();
+		if (href.contains("#") && this.findAncestor(Navbar.class) == null) {
+			final String[] parts = href.split("#");
+			if (parts.length > 0) {
+				final String target = parts[1];
+				this.anchor.setAttribute("data-toggle", "tab");
+				if (!target.equals(href)) {
+					this.anchor.setAttribute("data-target", "#" + target);
+				}
 			}
 		}
-		anchor.setAttribute("href", href);
-		super.appendChild(anchor);
+		this.anchor.setAttribute("href", href);
+		super.appendChild(this.anchor);
 	}
-	
+
+	@Override
+	public void applyLabel(String label) {
+		final Message message = new Message(label);
+		if (this.anchor != null) {
+			anchor.appendChild(message);
+		} else {
+			super.appendChild(message);
+		}
+	}
 }
