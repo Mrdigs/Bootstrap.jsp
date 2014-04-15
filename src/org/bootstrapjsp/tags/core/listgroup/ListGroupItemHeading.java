121ece85e69a5b6c6c0c07a271d66548b5aff0cd
Mrdigs <digger.scott@gmail.com>
2014-04-15 07:23:29 +0100

    Corrected wrong element and class on listgroup item heading

--- src/org/bootstrapjsp/tags/core/listgroup/ListGroupItemHeading.java
+++ src/org/bootstrapjsp/tags/core/listgroup/ListGroupItemHeading.java
@@ -7,22 +7,26 @@
 package org.bootstrapjsp.tags.core.listgroup;
 
 import org.bootstrapjsp.facet.LabelFacet;
-import org.bootstrapjsp.tags.html.Paragraph;
+import org.bootstrapjsp.tags.html.Heading;
+import org.tldgen.annotations.Attribute;
 import org.tldgen.annotations.Tag;
 
 @Tag(name="listgroupitemheading", dynamicAttributes=true)
-public class ListGroupItemHeading extends Paragraph {
+public class ListGroupItemHeading extends Heading {
 
 	public ListGroupItemHeading() {
-		super("list-group-item-text");
+		super(4, "list-group-item-heading");
 		super.addFacet(new LabelFacet());
 	}
 	
 	public ListGroupItemHeading(String label) {
 		this();
-		if (label != null) {
-			super.getFacet(LabelFacet.class).setValue(label);
-		}
+		super.setAttribute("label", label);
 	}
 	
+	@Override
+	@Attribute(rtexprvalue=true)
+	public void setLevel(int level) {
+		super.setLevel(level);
+	}
 }
