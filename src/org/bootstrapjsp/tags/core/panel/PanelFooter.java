23c22e5bb7d101e3fd37a3bab10a4a1e1fb66981
Mrdigs <digger.scott@gmail.com>
2014-04-17 17:45:37 +0100

    Improved documentation for <panelfooter>

--- src/org/bootstrapjsp/tags/core/panel/PanelFooter.java
+++ src/org/bootstrapjsp/tags/core/panel/PanelFooter.java
@@ -9,6 +9,20 @@ package org.bootstrapjsp.tags.core.panel;
 import org.bootstrapjsp.tags.html.Div;
 import org.tldgen.annotations.Tag;
 
+/**
+ * Wrap buttons or secondary text within a <i>&lt;panel&gt;</i> with a 
+ * <i>&lt;panelfooter&gt;</i>. Note that panel footers do not inherit colors 
+ * and borders when using contextual variations as they are not meant to be in
+ * the foreground.
+ * <p>
+ * <dl>
+ * <dt><b>Example</b></dt>
+ * <dd>&lt;b:panelfooter&gt;...&lt;/b:panelfooter&gt;</dd>
+ * <dt><b>Output</b></dt>
+ * <dd>&lt;div class="panel-footer"&gt;...&lt;/div&gt;</dd>
+ * </dl>
+ * </p>
+ */
 @Tag(name="panelfooter",dynamicAttributes=true)
 public class PanelFooter extends Div {
 
