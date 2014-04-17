4f27b9fa308016fd61525e6c82888ae3a0afcf5f
Mrdigs <digger.scott@gmail.com>
2014-04-17 17:26:49 +0100

    Improved documentation for <panel>

--- src/org/bootstrapjsp/tags/core/panel/Panel.java
+++ src/org/bootstrapjsp/tags/core/panel/Panel.java
@@ -13,6 +13,28 @@ import org.bootstrapjsp.tags.html.Div;
 import org.tldgen.annotations.BodyContent;
 import org.tldgen.annotations.Tag;
 
+/**
+ * While not always necessary, sometimes you need to put your DOM in a box. 
+ * For those situations, try the panel component.
+ * <p>
+ * By default, all the <i>&lt;panel&gt;</i> does is apply some basic border 
+ * and padding to contain some content.
+ * </p>
+ * <p>
+ * If you set a <i>label</i> attribute, a <i>&lt;panelheading&gt;</i> will
+ * automatically be added with the label text as the body, and a 
+ * <i>&lt;panelbody&gt;</i> will also be added to hold your body. If not,
+ * you will need to add your own heading and body.
+ * </p>
+ * <p>
+ * <dl>
+ * <dt><b>Example</b></dt>
+ * <dd>&lt;b:panel label="Heading"&gt;...&lt;/b:panel&gt;</dd>
+ * <dt><b>Output</b></dt>
+ * <dd>&lt;div class="panel"&gt;&lt;div class="panel-heading"&gt;...&lt;/div&gt;&lt;/div&gt;</dd>
+ * </dl>
+ * </p>
+ */
 @Tag(bodyContent=BodyContent.SCRIPTLESS,dynamicAttributes=true)
 public class Panel extends Div implements Labelable {
 	
