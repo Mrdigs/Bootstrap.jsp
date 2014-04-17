17fc21f79157b0d4153d1e2de4485eab0778e840
Mrdigs <digger.scott@gmail.com>
2014-04-17 17:14:14 +0100

    Improved documentation for <buttongroup>

--- src/org/bootstrapjsp/tags/core/button/ButtonGroup.java
+++ src/org/bootstrapjsp/tags/core/button/ButtonGroup.java
@@ -14,8 +14,20 @@ import org.bootstrapjsp.dialect.Html;
 import org.bootstrapjsp.facet.JustifiedFacet;
 import org.bootstrapjsp.facet.SizeFacet;
 import org.bootstrapjsp.tags.html.Div;
+import org.tldgen.annotations.Attribute;
 import org.tldgen.annotations.Tag;
 
+/**
+ * Group a series of buttons together on a single line with the button group.
+ * <p>
+ * <dl>
+ * <dt><b>Example</b></dt>
+ * <dd>&lt;b:buttongroup size="sm" vertical="true"&gt;... &lt;/b:buttongroup&gt;</dd>
+ * <dt><b>Output</b></dt>
+ * <dd>&lt;div class="btn-group-vertical btn-group-sm"&gt;... &lt;/div&gt;</dd>
+ * </dl>
+ * </p>
+ */
 @Tag(name="buttongroup",dynamicAttributes=true)
 public class ButtonGroup extends Div {
 
@@ -37,10 +49,19 @@ public class ButtonGroup extends Div {
 		super.doTag();
 	}
 	
+	/**
+	 * Make a set of buttons appear vertically stacked rather than 
+	 * horizontally.
+	 */
+	@Attribute(rtexprvalue=true)
 	public void setVertical(boolean vertical) {
 		this.vertical = vertical;
 	}
 
+	/**
+	 * Trigger dropdown menus above elements within the button group.
+	 */
+	@Attribute(rtexprvalue=true)
 	public void setDropup(boolean dropup) {
 		super.setAttribute(Html.CLASS_ATTRIBUTE, "dropup");
 	}
