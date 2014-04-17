6b2e0edbec658c2191445d7d70500f184da0434a
Mrdigs <digger.scott@gmail.com>
2014-04-17 17:34:03 +0100

    Improved documentation for <panelheading>

--- src/org/bootstrapjsp/tags/core/panel/PanelHeading.java
+++ src/org/bootstrapjsp/tags/core/panel/PanelHeading.java
@@ -11,6 +11,22 @@ import org.bootstrapjsp.facet.Labelable;
 import org.bootstrapjsp.tags.html.Div;
 import org.tldgen.annotations.Tag;
 
+/**
+ * Easily add a heading container to your panel with <i>&lt;panelheading&gt;</i>. 
+ * You may also include a <i>&lt;paneltitle&gt;</i> to add a pre-styled heading.
+ * <p>
+ * If you set a <i>label</i> attribute then a <i>&lt;paneltitle&gt;</i> will
+ * automatically be added for you.
+ * </p>
+ * <p>
+ * <dl>
+ * <dt><b>Example</b></dt>
+ * <dd>&lt;b:panelheading label="..."/&gt;</dd>
+ * <dt><b>Output</b></dt>
+ * <dd>&lt;div class="panel-heading"&gt;&lt;h3 class="panel-title"&gt;...&lt;/h3&gt;&lt;/div&gt;</dd>
+ * </dl>
+ * </p>
+ */
 @Tag(name="panelheading",dynamicAttributes=true)
 public class PanelHeading extends Div implements Labelable {
 
