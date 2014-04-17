442eae174561a82fe85f123acd575047bd54f0a2
Mrdigs <digger.scott@gmail.com>
2014-04-17 17:21:03 +0100

    Improved documentation for <container>

--- src/org/bootstrapjsp/tags/core/misc/Container.java
+++ src/org/bootstrapjsp/tags/core/misc/Container.java
@@ -17,6 +17,23 @@ import org.tldgen.annotations.Attribute;
 import org.tldgen.annotations.BodyContent;
 import org.tldgen.annotations.Tag;
 
+/**
+ * Easily center a page's contents by wrapping its contents in a &lt;container&gt;. 
+ * Containers set width at various media query breakpoints to match the grid 
+ * system.
+ * <p>
+ * Note that, due to padding and fixed widths, containers are not nestable, and 
+ * will throw an error if nested.
+ * </p>
+ * <p>
+ * <dl>
+ * <dt><b>Example</b></dt>
+ * <dd>&lt;b:container fluid="true"&gt;... &lt;/b:container&gt;</dd>
+ * <dt><b>Output</b></dt>
+ * <dd>&lt;div class="container-fluid"&gt;... &lt;/div&gt;</dd>
+ * </dl>
+ * </p>
+ */
 @Tag(bodyContent=BodyContent.SCRIPTLESS,dynamicAttributes=true)
 public class Container extends Div {
 
@@ -40,6 +57,9 @@ public class Container extends Div {
 		super.setParent(parent);
 	}
 	
+	/**
+	 * Makes the container fluid.
+	 */
 	@Attribute(rtexprvalue=true)
 	public void setFluid(boolean fluid) {
 		this.fluid = fluid;
