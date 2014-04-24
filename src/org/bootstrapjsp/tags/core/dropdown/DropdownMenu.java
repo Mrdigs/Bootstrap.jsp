ff027fb765357b15d54c079114267c56a05924a4
Mrdigs <digger.scott@gmail.com>
2014-04-24 08:11:03 +0100

    Improved documentation for <dropdownmenu>

--- src/org/bootstrapjsp/tags/core/dropdown/DropdownMenu.java
+++ src/org/bootstrapjsp/tags/core/dropdown/DropdownMenu.java
@@ -7,13 +7,19 @@
 package org.bootstrapjsp.tags.core.dropdown;
 
 import org.bootstrapjsp.dialect.Html;
+import org.bootstrapjsp.facet.AlignFacet;
 import org.bootstrapjsp.tags.html.List;
 import org.tldgen.annotations.Tag;
 
 /**
  * A Dropdown Menu.
  * <p>
- * &lt;ul class="dropdown-menu" role="menu"&gt;...&lt;/ul&gt;
+ * <dl>
+ * <dt><b>Example</b></dt>
+ * <dd>&lt;dropdownmenu pull="right"&gt;...&lt;/dropdownmenu&gt;</dd>
+ * <dt><b>Output</b></dt>
+ * <dd>&lt;ul class="dropdown-menu dropdown-menu-right" role="menu"&gt;...&lt;/ul&gt;</dd>
+ * </dl>
  * </p>
  */
 @Tag(name="dropdownmenu",dynamicAttributes=true)
@@ -22,5 +28,6 @@ public class DropdownMenu extends List {
 	public DropdownMenu() {
 		super(false, "dropdown-menu");
 		super.setAttribute(Html.ROLE_ATTRIBUTE, "menu");
+		super.addFacet(new AlignFacet("dropdown-menu"));
 	}
 }
