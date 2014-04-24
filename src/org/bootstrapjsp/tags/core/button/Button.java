6f14abefac76bfe7fc7976dd73972bd66f768b1c
Mrdigs <digger.scott@gmail.com>
2014-04-24 07:28:46 +0100

    Improved documentation for <button> and added active and disabled facets

--- src/org/bootstrapjsp/tags/core/button/Button.java
+++ src/org/bootstrapjsp/tags/core/button/Button.java
@@ -13,7 +13,9 @@ import javax.servlet.jsp.tagext.JspTag;
 
 import org.bootstrapjsp.dialect.Html;
 import org.bootstrapjsp.exception.InvalidAttributeException;
+import org.bootstrapjsp.facet.ActiveFacet;
 import org.bootstrapjsp.facet.ContextFacet;
+import org.bootstrapjsp.facet.DisabledFacet;
 import org.bootstrapjsp.facet.IconFacet;
 import org.bootstrapjsp.facet.JustifiedFacet;
 import org.bootstrapjsp.facet.LabelFacet;
@@ -28,8 +30,17 @@ import org.tldgen.annotations.Attribute;
 import org.tldgen.annotations.Tag;
 
 /**
- * I am a description
- * 
+ * A button. Buttons are either a <i>&lt;button&gt;</i> or a <i>&lt;a&gt;</i>
+ * element depending on whether you give the button an <i>"href"</i> attribute
+ * or not.
+ * <p>
+ * <dl>
+ * <dt><b>Example</b></dt>
+ * <dd>&lt;button context="primary"&gt;... &lt;/button&gt;</dd>
+ * <dt><b>Output</b></dt>
+ * <dd>&lt;button class="btn btn-primary"&gt;... &lt;/button&gt;</dd>
+ * </dl>
+ * </p>
  */
 @Tag(dynamicAttributes=true)
 public class Button extends Component implements Toggleable, Moldable {
@@ -41,6 +52,9 @@ public class Button extends Component implements Toggleable, Moldable {
 		super.addFacet(new ContextFacet("btn", "default"));
 		super.addFacet(new IconFacet());
 		super.addFacet(new LabelFacet());
+		super.addFacet(new ActiveFacet(false));
+		// TODO Add JavaScript support for disabling clicks on disabled <a> buttons
+		super.addFacet(new DisabledFacet(false));
 	}
 	
 	@Override
@@ -58,23 +72,47 @@ public class Button extends Component implements Toggleable, Moldable {
 		super.doTag();
 	}
 	
+	/**
+	 * Setting an <i>"href"</i> on a button will change the button to an anchor
+	 * so it can be clicked on to trigger navigation to the given URL.
+	 */
 	@Attribute(rtexprvalue=true)
 	public void setHref(String href) {
 		super.setAttribute("href", href);
 		super.setAttribute(Html.ROLE_ATTRIBUTE, "button");
+		// TODO Only <button> elements are supported within nav and
+		// navbar - add a check and error if used in this way.
 		super.setElement("a");
 	}
-	
+
+	/**
+	 * Sets the <i>"data-dismiss"</i> attribute of the button. Any value is 
+	 * allowed, there is no validation.
+	 */
 	@Attribute(rtexprvalue=true)
 	public void setDismiss(String dismiss) {
-		super.setAttribute("data-dismiss", dismiss);
+		if (dismiss != null) {
+			super.setAttribute("data-dismiss", dismiss);
+		}
 	}
 
+	/**
+	 * Sets the <i>"data-target"</i> attribute of the button. The given value is
+	 * prepended with a hash for you if one is not given. Absolute URLs are not
+	 * supported.
+	 */
 	@Attribute(rtexprvalue=true)
 	public void setTarget(String target) {
-		super.setAttribute("data-target", "#" + target);
+		if (target != null) {
+			final String value = target.startsWith("#") ? target : "#" + target;
+			super.setAttribute("data-target", value);
+		}
 	}
 
+	/**
+	 * Setting <i>block</i> to "true" will cause the button to be a block level
+	 * button, spanning the full width of the parent.
+	 */
 	@Attribute(rtexprvalue=true)
 	public void setBlock(boolean block) {
 		if (block) {
