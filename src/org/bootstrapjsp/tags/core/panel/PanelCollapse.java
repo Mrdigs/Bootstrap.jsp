2689c37ecbdce148d83c2f445c7ca248d40af094
Mrdigs <digger.scott@gmail.com>
2019-06-06 08:55:56 +0100

    Added panel-collapse tag

---
+++ src/org/bootstrapjsp/tags/core/panel/PanelCollapse.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2014 Darren Scott - All Rights Reserved
+ * 
+ * This program is distributed under LGPL Version 2.1 in the hope that
+ * it will be useful, but WITHOUT ANY WARRANTY.
+ */
+package org.bootstrapjsp.tags.core.panel;
+
+import org.bootstrapjsp.tags.html.Div;
+import org.tldgen.annotations.Tag;
+
+@Tag(name="panelcollapse",dynamicAttributes=true)
+public class PanelCollapse extends Div {
+	
+	public PanelCollapse() {
+		super("panel-collapse collapse");
+	}
+}
